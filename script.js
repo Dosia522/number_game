8d77d2544b720a9ee8307c245899ca35ff1c56de
Alexy <alexy.bunin@mail.ru>
2020-11-21 12:50:43 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 "use strict"
 
-let money = 10;
-let income = 20;
-let addExpenses = 15;
-let deposit = 1;
-let mission = 25;
-let period = 15;
-
 alert("Hello world!");
 
-console.log("Здравствуйте");
+console.log("Здравствуйте"); 
+
+let money = 40000;
+console.log(typeof money);
+
+let income = '10000';
+console.log(typeof income);
+
+let addExpenses = '10000, 1000, 5000';
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let deposit = true;
+console.log(typeof deposit);
+
+let mission = 50000;
+console.log('Цель заботать ' + mission + ' рублей');
+
+let period = 6;
+console.log('Период =' + ' ' + period + ' месяцев');
+
+let budgetDay = 40000;
+console.log('budgetDay: ', budgetDay);
+
+
+
+
+
